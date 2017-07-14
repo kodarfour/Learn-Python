ba969f2b47121483b0f5606b15bdd308959331f8
KD <kodarfour@gmail.com>
2017-07-14 18:55:24 -0400

    added elifs and more comments

--- bin/Chapter4.py
+++ bin/Chapter4.py
@@ -15,7 +15,7 @@ if muscular:
 	#this is if it is muscular AND hairy
 		print('Found a bear.')
 	else:
-	#this is if its JUST muscular	
+	#this is if its JUST muscular   
 		print('That\'s a bodybuilder.')
 else: 
 	if hairy:
@@ -23,10 +23,34 @@ else:
 		print('It\'s a hamster.')
 	else:
 	#this is if its NEITHER muscular OR hairy
-		print('EWW! There is a naked mole-rat!')	
+		print('EWW! There is a naked mole-rat!')    
+
+#elifs are used when there are more than two possibilities.
+#they basically act as a shorter version of use else: if: therefore having the
+#name elif (this is put into my terms of understanding)
+
+#These are all of the comparison operators
+# == 'equality'
+# != 'inequality'
+# < 'less than'
+# <= 'less than or equal'
+# > 'greater than
+# >= 'greater than or equal'
+# in 'membership'
+
+beverage = 'butterbeer'
+if beverage == 'orange juice': 
+	print('Oh, it must be for breakfast.\n')
+elif beverage == 'water':
+	print('Great, you\'re staying hydrated.\n')
+elif beverage == 'soda':
+	print('Don\'t drink to much! It rots your teeth.\n')
+elif beverage == 'hennesey':
+	print('That is QUITE expensive mate.\n')
+else:
+	print('I\'ve never heard of this drink. It must be magical.\n')
 
 print('\nRepeat with while:\n')
-
 
 count = 1
 while count <= 9:
@@ -35,7 +59,7 @@ while count <= 9:
 	print(count)
 	count += 2
 	# incremented the value by +2
-print("Python Compiler: Finished!")
+print("Python Compiler: Finished!\n")
 
 while True:
 	rdm = input("Automatic String Capitalizer, Try It! (type q to quit): ")
